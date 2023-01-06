0f58e35d52c74384e96391d7dbfebc406ef78917
Hemant Kothari <36153698+hemant7405@users.noreply.github.com>
2023-01-06 16:25:15 +0530

    Create social.js

---
+++ social.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+
+var socialblock = '<section id="social-boxes" class="section-inverse" style="display:nonex;">' +
+'<div class="row eq-height no-gutters" >' +
+    '<div class="col-xs-12">' +
+        '<div id="social-header" class="col-xs-12 col-md-3 p-x-2 p-y-3" data-aos="fade-right">' +
+            '<div class="border-indent-title text-center text-uppercase m-b-2">' +
+                '<h1>' +
+                    '<span data-aos="fade-left">Let&#8217;s Get</span>' +
+                    '<span data-aos="fade-right">Social</span>' +
+                '</h1>' +
+            '</div>' +
+        '</div>' +
+        '<div class="social-links col-xs-12 col-md-9 p-x-0">' +
+            '<div class="row eq-height no-gutters">' +
+                '<div class="col-xs-5ths" style="width:20%;">' +
+                    '<a href="https://www.facebook.com/" target="_blank" data-aos="fade-left" data-aos-delay="100" rel="noopener">' +
+                            '<i class="fa fa-facebook-official"></i>' +
+                            '<img class="img-fluid" src="https://kvtemplates.dev/chunk/wp-content/uploads/sites/86/2022/08/Depositphotos_22319009_ds.jpg" alt="facebook">' +
+                        '</a>' +
+                '</div>' +
+                '<div class="col-xs-5ths" style="width:20%;">' +
+                    '<a href="https://www.instagram.com/" target="_blank" data-aos="fade-left" data-aos-delay="300" rel="noopener">' +
+                            '<i class="fa fa-instagram"></i>' +
+                             '<img class="img-fluid" src="https://kvtemplates.dev/chunk/wp-content/uploads/sites/86/2022/08/vita-vilcina-KtOid0FLjqU-unsplash.jpg" alt="instagram">' +
+                        '</a>' +
+                '</div>' +
+                '<div class="col-xs-5ths" style="width:20%;">' +
+                    '<a href="https://www.youtube.com/" target="_blank" data-aos="fade-left" data-aos-delay="500" rel="noopener">' +
+                            '<i class="fa fa-youtube"></i>' +
+                             '<img class="img-fluid" src="https://kvtemplates.dev/chunk/wp-content/uploads/sites/86/2022/08/Depositphotos_19842517_xl-2015.jpg" alt="youtube">' +
+                        '</a>' +
+                '</div>' +
+                '<div class="col-xs-5ths" style="width:20%;">' +
+                    '<a href="https://www.google.com/maps/place/Justin+Hemker,+Realtor+at+The+Namas+Team,+brokered+by+EXP+Realty" target="_blank" data-aos="fade-left" data-aos-delay="700" rel="noopener">' +
+                            '<i class="fa fa-google"></i>' +
+                             '<img class="img-fluid" src="https://kvtemplates.dev/chunk/wp-content/uploads/sites/86/2022/08/Depositphotos_274559848_xl-2015.jpg" alt="google">' +
+                        '</a>' +
+                '</div>' +
+                '<div class="col-xs-5ths" style="width:20%;">' +
+                    '<a href="https://www.linkedin.com/" target="_blank" data-aos="fade-left" data-aos-delay="900" rel="noopener">' +
+                            '<i class="fa fa-linkedin"></i>' +
+                             '<img class="img-fluid" src="https://kvtemplates.dev/chunk/wp-content/uploads/sites/86/2022/08/Depositphotos_76073721_xl-2015.jpg" alt="linkedin">' +
+                        '</a>' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+'</div>' +
+'</section>';
+
+document.getElementById("wrapper").innerHTML = socialblock;
+});
